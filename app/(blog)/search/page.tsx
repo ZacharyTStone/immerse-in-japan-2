f6e9f6089b07f6666027b2a40cbcc7c7a8ce770e
ZacharyTStone <Zach.Stone.Developer@gmail.com>
2024-05-26 15:42:38 -0400

    add suspense

--- app/(blog)/search/page.tsx
+++ app/(blog)/search/page.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 import { Image } from "next-sanity/image";
 import { urlForImage } from "@/sanity/lib/utils";
+import { Suspense } from "react";
 
 interface Post {
   _id: string;
@@ -87,64 +88,66 @@ export default function SearchPage() {
   };
 
   return (
-    <div className="container mx-auto px-5 py-10">
-      <form
-        method="GET"
-        action="/search"
-        onSubmit={handleSearch}
-        className="flex justify-center mb-10"
-      >
-        <input
-          type="text"
-          name="query"
-          defaultValue={searchQuery}
-          placeholder="Search by title"
-          className="border p-2 w-1/2"
-        />
-        <button type="submit" className="ml-2 p-2 bg-blue-500 text-white">
-          Search
-        </button>
-      </form>
-      <div className="mt-5 space-y-5">
-        {posts.map((post) => {
-          console.log("post", post);
-          const picture = post.coverImage;
-          return (
-            <div key={post._id} className="border p-4 rounded shadow flex">
-              {post.coverImage && (
-                <div className="mr-4">
-                  <Image
-                    alt={picture?.alt || ""}
-                    className="h-full rounded-full object-cover"
-                    height={100}
-                    width={120}
-                    src={
-                      urlForImage(picture)
-                        ?.height(200)
-                        .width(200)
-                        .fit("crop")
-                        .url() as string
-                    }
-                  />
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="container mx-auto px-5 py-10">
+        <form
+          method="GET"
+          action="/search"
+          onSubmit={handleSearch}
+          className="flex justify-center mb-10"
+        >
+          <input
+            type="text"
+            name="query"
+            defaultValue={searchQuery}
+            placeholder="Search by title"
+            className="border p-2 w-1/2"
+          />
+          <button type="submit" className="ml-2 p-2 bg-blue-500 text-white">
+            Search
+          </button>
+        </form>
+        <div className="mt-5 space-y-5">
+          {posts.map((post) => {
+            console.log("post", post);
+            const picture = post.coverImage;
+            return (
+              <div key={post._id} className="border p-4 rounded shadow flex">
+                {post.coverImage && (
+                  <div className="mr-4">
+                    <Image
+                      alt={picture?.alt || ""}
+                      className="h-full rounded-full object-cover"
+                      height={100}
+                      width={120}
+                      src={
+                        urlForImage(picture)
+                          ?.height(200)
+                          .width(200)
+                          .fit("crop")
+                          .url() as string
+                      }
+                    />
+                  </div>
+                )}
+                <div>
+                  <a
+                    href={`/posts/${post.slug.current}`}
+                    className="text-xl font-bold text-blue-600 hover:underline"
+                    target="_blank"
+                  >
+                    {post.title}
+                  </a>
+                  <p className="text-gray-700 mt-2">{post.excerpt}</p>
+                  <p className="text-gray-500 text-sm mt-1">
+                    Posted on: {new Date(post.date).toLocaleDateString()}
+                  </p>
                 </div>
-              )}
-              <div>
-                <a
-                  href={`/posts/${post.slug.current}`}
-                  className="text-xl font-bold text-blue-600 hover:underline"
-                  target="_blank"
-                >
-                  {post.title}
-                </a>
-                <p className="text-gray-700 mt-2">{post.excerpt}</p>
-                <p className="text-gray-500 text-sm mt-1">
-                  Posted on: {new Date(post.date).toLocaleDateString()}
-                </p>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
